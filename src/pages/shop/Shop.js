3572f3341d153f69ef13ac94823869d236890f57
Felipe <fb.dc.col@gmail.com>
2020-05-30 12:27:22 -0500

    update shop.data

---
+++ src/pages/shop/Shop.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components'
+
+class Shop extends React.Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+    
+  render() {
+      return (
+        <ShopStyle>
+    
+        </ShopStyle>
+      );
+  }  
+}
+
+export default Shop;
+
+
+const ShopStyle = styled.div`
+
+`
