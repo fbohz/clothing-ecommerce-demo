092b823a85f36a48542cffa7f7d3872367def4e2
Felipe <fb.dc.col@gmail.com>
2020-06-03 12:51:29 -0500

    ponyo img src fix

--- src/lib/shop.data.js
+++ src/lib/shop.data.js
@@ -178,7 +178,7 @@ export const SHOP_DATA =  {
           {
             id: 21,
             name: 'Ponyo',
-            imageUrl: 'https://www.studioghibli.com.au/wp-content/uploads/2017/07/2783-title-treatment-portrait-key-art-normal-medium-683x1024.jpg',
+            imageUrl: 'https://www.studioghibli.com.au/wp-content/uploads/2017/07/14815-title-treatment-portrait-key-art-normal-medium-683x1024.jpg',
             price: 18,
             description: "The son of a sailor, 5-year old Sosuke lives a quiet life on an oceanside cliff with his mother Lisa. One fateful day, he finds a beautiful goldfish trapped in a bottle on the beach and upon rescuing her, names her Ponyo. But she is no ordinary goldfish. The daughter of a masterful wizard and a sea goddess, Ponyo uses her father's magic to transform herself into a young girl and quickly falls in love with Sosuke, but the use of such powerful sorcery causes a dangerous imbalance in the world. As the moon steadily draws nearer to the earth and Ponyo's father sends the ocean's mighty waves to find his daughter, the two children embark on an adventure of a lifetime to save the world and fulfill Ponyo's dreams of becoming human.",
             director: "Hayao Miyazaki",
