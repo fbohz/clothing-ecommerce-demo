e7479c5157f474bcf4b99c0bf272437f5f1b592f
Felipe <fb.dc.col@gmail.com>
2020-06-12 22:06:39 -0500

    login signup responsiveness

--- frontend/src/pages/users/LoginLogout.js
+++ frontend/src/pages/users/LoginLogout.js
@@ -20,4 +20,13 @@ const LoginLogoutStyle = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
+  @media screen and (max-width: 800px) {
+    display: block;
+    flex-direction: column;
+    width: unset;
+    align-items: center;
+    > *:first-child {
+      margin-bottom: 50px;
+    }
+  }
 `
